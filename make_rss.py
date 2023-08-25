f9b6b56251e236aa775fa9cdc3537c979195f3ec
Danielle Jenkins <git@d6e.io>
2023-08-25 01:27:36 -0700

    Update rss generation

--- make_rss.py
+++ make_rss.py
@@ -1,9 +1,12 @@
 import os
+import eyed3
+from urllib.parse import urljoin
 from xml.etree.ElementTree import Element, SubElement, tostring
 from xml.dom import minidom
+import urllib.parse
 
 
-def generate_podcast_feed(book_title, mp3_dir, text_chapters_dir, base_url):
+def generate_podcast_feed(book_title, input_dir, output_dir, base_url):
     rss = Element("rss", version="2.0")
     channel = SubElement(rss, "channel")
 
@@ -16,29 +19,37 @@ def generate_podcast_feed(book_title, mp3_dir, text_chapters_dir, base_url):
     description = SubElement(channel, "description")
     description.text = f"A TTS-generated audiobook of {book_title}"
 
-    for filename in os.listdir(mp3_dir):
-        if not filename.endswith(".mp3"):
+    mp3_files = sorted(os.listdir(input_dir), key=lambda x: int(x.split('_')[1].split('.')[0]))
+
+    for idx, mp3_filename in enumerate(mp3_files, start=1):
+        if not mp3_filename.endswith('.mp3'):
             continue
-        chapter_number = filename.split('_')[1].split('.')[0]
-        text_filename = filename.replace('.mp3', '.txt')
-        with open(os.path.join(text_chapters_dir, text_filename), 'r', encoding='utf-8') as file:
-            chapter_title = file.readline().strip().replace("Chapter ", "")
-        mp3_url = os.path.join(base_url, filename)
+
+        audiofile = eyed3.load(os.path.join(input_dir, mp3_filename))
+        duration = int(audiofile.info.time_secs)
+
+        text_filename = mp3_filename.replace('.mp3', '.txt')
+        with open(os.path.join(output_dir, text_filename), 'r', encoding='utf-8') as file:
+            chapter_title = file.readline().strip()
+
+        mp3_url_path = mp3_filename.replace('\\', '/')
+        mp3_url = urljoin(base_url, mp3_url_path)
 
         item = SubElement(channel, "item")
         item_title = SubElement(item, "title")
-        item_title.text = f"Chapter {chapter_number}: {chapter_title}"
+        item_title.text = chapter_title
 
         item_link = SubElement(item, "link")
         item_link.text = mp3_url
 
-        enclosure = SubElement(item, "enclosure", url=mp3_url, type="audio/mpeg")
+        SubElement(item, "enclosure", url=mp3_url, type="audio/mpeg", length=str(duration))
 
         item_description = SubElement(item, "description")
         item_description.text = f"{book_title} - {chapter_title}"
 
     xml_str = minidom.parseString(tostring(rss)).toprettyxml(indent="   ")
-    with open("feed.xml", "w", encoding='utf-8') as f:
+    filename = "feed.xml"
+    with open(filename, "w", encoding='utf-8') as f:
         f.write(xml_str)
-
+    print(f"Podcast feed for '{book_title}' created here: '{filename}'")
 
